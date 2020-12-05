576443633bab3f14bdb4bb61e95536ff2837ed36
naman-adep <67358247+naman-adep@users.noreply.github.com>
2020-12-05 08:50:15 +0530

    Add files via upload

---
+++ location.py
@@ -0,0 +1,12 @@
+import pyautogui, sys, time
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X:' + str(x).rjust(4) + ' Y:' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print('\n')
